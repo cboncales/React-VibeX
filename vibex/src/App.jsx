5eca8f63ffcc64dcc679513158ea2bd44ffc1bd8
cboncales <clarkboncales115@gmail.com>
2025-01-29 16:51:25 +0800

    created dark mode feature

--- vibex/src/App.jsx
+++ vibex/src/App.jsx
@@ -2,16 +2,25 @@ import Friends from "./components/Friends";
 import Feed from "./components/Feed";
 import Navigation from "./components/Navigation";
 import BottomNav from "./components/BottomNav";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+    document.documentElement.classList.toggle("dark", !darkMode);
+  };
   return (
     <>
-      <div className="flex min-h-screen w-full items-center justify-center md:flex-row">
+      <div
+        className={`flex min-h-screen w-full items-center justify-center md:flex-row ${darkMode ? "bg-black" : "bg-white"}`}
+      >
         <main className="flex min-h-screen w-full">
-          <Navigation />
+          <Navigation toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
           <Feed />
           <Friends />
-          <BottomNav />
+          <BottomNav toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
         </main>
       </div>
     </>
